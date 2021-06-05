e3e091e8410dda1e812970a713c802f30a15aca5
joerkig <44418429+joerkig@users.noreply.github.com>
2021-06-05 22:09:33 +0200

    Initial Commit

---
+++ GottaGoFast/GottaGoFast.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GottaGoFast
+{
+    public class MyMod : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            MelonLogger.Msg("OnSceneWasLoaded was called");
+            GameObject sliderVelocity = GameObject.Find("GameSetting/SliderVelocity/Slider");
+            if (sliderVelocity != null)
+            {
+                Slider slider = sliderVelocity.GetComponent<Slider>();
+                if (slider != null)
+                {
+                    slider.maxValue = 12;
+                    slider.minValue = -7;
+                }
+                else
+                {
+                    MelonLogger.Msg("OSWL Failed to get Component");
+                }
+            }
+            else
+            {
+                MelonLogger.Msg("OSWL Failed to find GameObject");
+            }
+        }
+        public override void OnApplicationStart()
+        {
+
+        }
+        public override void OnApplicationQuit()
+        {
+
+        }
+    }
+}
---
+++ GottaGoFast/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using GottaGoFast;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: MelonInfo(typeof(MyMod), "GottaGoFast", "1.0.0", "joerkig")]
+[assembly: MelonGame("Odders", "OhShape")]
+[assembly: AssemblyTitle("GottaGoFast")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GottaGoFast")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eba1ab5e-62da-46b3-a952-14f2e527a0ce")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
