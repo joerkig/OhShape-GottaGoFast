891edbb6aebfd31ff3b20749589f5d60cec52ca4
joerkig <44418429+joerkig@users.noreply.github.com>
2021-06-05 23:15:26 +0200

    Force GodMode
    Forces GodMode on when using values normally not possible.

--- GottaGoFast/GottaGoFast.cs
+++ GottaGoFast/GottaGoFast.cs
@@ -11,6 +11,51 @@ namespace GottaGoFast
 {
     public class MyMod : MelonMod
     {
+        public override void OnLateUpdate()
+        {
+            GameObject godMode = GameObject.Find("GameSetting/GodMode/GodModeToggle");
+            if (godMode != null)
+            {
+                Toggle godModeToggle = godMode.GetComponent<Toggle>();
+                if (godModeToggle != null)
+                {
+                    GameObject sliderVelocity = GameObject.Find("GameSetting/SliderVelocity/Slider");
+                    if (sliderVelocity != null)
+                    {
+                        Slider slider = sliderVelocity.GetComponent<Slider>();
+                        if (slider != null)
+                        {
+                            if (slider.value < 0)
+                            {
+                                godModeToggle.isOn = true;
+                                GameObject scoreMultiplier = GameObject.Find("ScoreMultiplier");
+                                if (scoreMultiplier != null)
+                                {
+                                    Text scoreText = scoreMultiplier.GetComponent<Text>();
+                                    if (scoreText != null)
+                                    {
+                                        scoreText.text = "No Fail Forced By Mod";
+                                    }
+                                }
+                            }
+                            else if (slider.value > 4)
+                            {
+                                godModeToggle.isOn = true;
+                                GameObject scoreMultiplier = GameObject.Find("ScoreMultiplier");
+                                if (scoreMultiplier != null)
+                                {
+                                    Text scoreText = scoreMultiplier.GetComponent<Text>();
+                                    if (scoreText != null)
+                                    {
+                                        scoreText.text = "No Fail Forced By Mod";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             MelonLogger.Msg("OnSceneWasLoaded was called");
